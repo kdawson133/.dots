62d822e233ffa6e2ad2b434f2fd59d0d67088f7c
kdawson133 <kdawson133@startmail.com>
2026-08-27 14:20:24 +1000

    updated

--- scripts/void/10-niri-dms.sh
+++ scripts/void/10-niri-dms.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # Add dms repo
-# echo "repository=https://void.danklinux.com/dms/current" | sudo tee /etc/xbps.d/dms.conf
-# echo "repository=https://void.danklinux.com/danklinux/current" | sudo tee /etc/xbps.d/danklinux.conf
-
-echo "repository=https://repo.voiders.dev" | sudo tee /etc/xbps.d/10-voiders-community.conf
+echo "repository=https://void.danklinux.com/dms/current" | sudo tee /etc/xbps.d/dms.conf
+echo "repository=https://void.danklinux.com/danklinux/current" | sudo tee /etc/xbps.d/danklinux.conf
 
 sudo xbps-install -Su
 
 # Install Desktop Packages
-sudo xbps-install niri noctalia noctalia-greeter dex greetd fuzzel alacritty power-profiles-daemon xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal gnome-keyring gnome-themes-standard gnome-themes-extra polkit-gnome nautilus fastfetch xwayland-satellite firefox
+sudo xbps-install niri dms dms-greeter danksearch dankcalendar dex greetd fuzzel alacritty power-profiles-daemon xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal gnome-keyring gnome-themes-standard gnome-themes-extra polkit-gnome nautilus fastfetch xwayland-satellite firefox
 
 # Enable Power Profile Daemon
 sudo ln -s /etc/sv/power-profiles-daemon /var/service
