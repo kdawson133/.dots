9bbfe3cb124a2452f364abcc97aef9fcc0dd74f5
kdawson133 <kdawson133@startmail.com>
2026-08-28 10:20:21 +1000

    updated

--- scripts/fed/00-rpm-fusion.sh
+++ scripts/fed/00-rpm-fusion.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo dnf install \ https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-
-sudo dnf install \ https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf makecache
 sudo dnf repolist
